b193f2efc01eba31c4d20ea791b1469963c30dbb
Tomy <tomy.prs25@gmail.com>
2020-11-23 07:41:22 +0700

    updater: AuroraBot
    Signed-off-by: Tomy <tomy.prs25@gmail.com>

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -142,7 +142,7 @@ async def update(event, repo, ups_rem, ac_br):
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
     await event.edit(
-        "`Successfully Updated!\n" "ProjectAlf is restarting... Wait for a second!`"
+        "`Successfully Updated!\n" "System is restarting... Wait for a second!`"
     )
     await asyncio.sleep(15)
     await event.delete()
